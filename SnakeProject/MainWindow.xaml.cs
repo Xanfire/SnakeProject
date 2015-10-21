1d55269c05e5dbf17ce0a69126e21a42f1a25e17
Lorenzo Polverelli <xanfire13@gmail.com>
2015-10-21 20:02:51 +0200

    Comandi Snake
    Ora Snake sarà in grado di muoversi in qualsiasi direzione. L'utente
    dovrà premere le frecce della tastiera per direzionare Snake.

--- SnakeProject/MainWindow.xaml.cs
+++ SnakeProject/MainWindow.xaml.cs
@@ -23,12 +23,23 @@ namespace SnakeProject
     public partial class MainWindow : Window
     {
         DispatcherTimer timer;
+        Image tail;
+        string direction = "";
         public MainWindow()
         {
             InitializeComponent();
+            /*
+            tail = new Image();
+            tail.Margin = new Thickness(30, 150, 0, 0);
+            tail.Height = 10;
+            tail.Width = 10;
+            tail.IsVisible = Visibility.Visible;
+            */
             timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromMilliseconds(100);
             timer.Tick += Timer_Tick;
+
+            this.KeyUp += new KeyEventHandler(OnButtonKeyDown);
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -38,15 +49,84 @@ namespace SnakeProject
               + ";component/"
               + "White.png", UriKind.Absolute));
 
+            tail.Source = new BitmapImage(new Uri(@"pack://application:,,,/"
+              + Assembly.GetExecutingAssembly().GetName().Name
+              + ";component/"
+              + "White.png", UriKind.Absolute));
+
             timer.Start();
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            if (Head.Margin.Left < 490)
+            switch (direction)
             {
-                Head.Margin = new Thickness(Head.Margin.Left + 10, Head.Margin.Top, 0, 0);
+                case "down":
+                    if (Head.Margin.Top < 490)
+                    {
+                        Head.Margin = new Thickness(Head.Margin.Left, Head.Margin.Top + 10, 0, 0);
+                    }
+                    break;
+
+                case "up":
+                    if (Head.Margin.Top >= 10)
+                    {
+                        Head.Margin = new Thickness(Head.Margin.Left, Head.Margin.Top -10, 0, 0);
+                    }
+                    break;
+
+                case "left":
+                    if (Head.Margin.Left >= 10)
+                    {
+                        Head.Margin = new Thickness(Head.Margin.Left - 10, Head.Margin.Top, 0, 0);
+                    }
+                    break;
+
+                case "right":
+                    if (Head.Margin.Left < 490)
+                    {
+                        Head.Margin = new Thickness(Head.Margin.Left + 10, Head.Margin.Top, 0, 0);
+                    }
+                    break;
+                default:
+                    if (Head.Margin.Left < 490)
+                    {
+                        Head.Margin = new Thickness(Head.Margin.Left + 10, Head.Margin.Top, 0, 0);
+                    }
+                    break;
             }
+            
+        }
+
+        private void OnButtonKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Down:
+                    if (direction != "down") {
+                        direction = "down";
+                    }
+                    break;
+                case Key.Up:
+                    if (direction != "up")
+                    {
+                        direction = "up";
+                    }
+                    break;
+                case Key.Left:
+                    if (direction != "left")
+                    {
+                        direction = "left";
+                    }
+                    break;
+                case Key.Right:
+                    if (direction != "right")
+                    {
+                        direction = "right";
+                    }
+                    break;
+            }
+
         }
     }
 }
